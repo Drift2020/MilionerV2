7360c2021b8faf3f1859b2cb6c6bb0e63fa77f3e
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-11-29 10:40:08 +0200

    ferst

---
+++ Milioners/Interfese/IDell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milioners
+{
+    public interface IDell
+    {
+        event MyDelegate ev;
+    }
+}
